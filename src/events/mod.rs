8907f571c7fdbb4ad777b04c23d9d2d5b1cc9626
970037201 <ericpetersen3.14@gmail.com>
2022-06-19 21:27:28 +0100

    barebones

---
+++ src/events/mod.rs
@@ -0,0 +1,10 @@
+use serenity::{
+    async_trait,
+    client::EventHandler,
+};
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod events;
+
+use events::Handler;
+use serenity::{prelude::GatewayIntents, Client};
+
+use std::{error::Error, fs::read_to_string};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let token_str = read_to_string("token/token.txt")?;
+    let intents = GatewayIntents::all();
+    let mut client = Client::builder(&token_str.trim(), intents)
+        .event_handler(Handler)
+        .await?;
+    client.start_shards(num_cpus::get() as u64).await?;
+    Ok(())
 }
